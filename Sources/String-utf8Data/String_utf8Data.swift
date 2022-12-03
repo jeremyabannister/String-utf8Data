af34c29e3abefb0b8f48ba4d29b6942609da9a2a
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2022-12-03 19:16:57 +0100

    Added `String.utf8Data`.

--- Sources/String-utf8Data/String_utf8Data.swift
+++ Sources/String-utf8Data/String_utf8Data.swift
@@ -1,6 +1,27 @@
-public struct String_utf8Data {
-    public private(set) var text = "Hello, World!"
+//
+//  String_utf8Data.swift
+//  
+//
+//  Created by Jeremy Bannister on 12/3/22.
+//
 
-    public init() {
+///
+@_exported import Foundation
+
+///
+public extension String {
+    
+    /// Encodes this `String` to `Data` using UTF8.
+    ///
+    /// This is a shortcut for the slightly more verbose:
+    /// ```
+    /// self.data(using: .utf8)!
+    /// ```
+    /// the permanent safety of which I first read about in this article:
+    /// https://www.objc.io/blog/2018/02/13/string-to-data-and-back/
+    /// and subsequently deemed to be sound and unexpiring advice, given the explicit fact that UTF8 is the permanently (ABI-related) preferred encoding for `Swift.String`, as stated in this article on swift.org:
+    /// https://www.swift.org/blog/utf8-string/
+    var utf8Data: Data {
+        self.data(using: .utf8)!
     }
 }
---
+++ Tests/String-utf8Data-tests/_exported_imports.swift
@@ -0,0 +1,9 @@
+//
+//  _exported_imports.swift
+//  
+//
+//  Created by Jeremy Bannister on 12/3/22.
+//
+
+///
+@_exported import String_utf8Data
